7b357017733df5779890f163ac75c5f8f31e14d8
commanderjcc <jj1355810@gmail.com>
2020-02-27 07:29:32 -0500

    init project

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// Name: Joshua Christensen
+// Project: Published/Calendar
+// File: main.cpp
+// Purposes: Make a Calendar from a given date
+// Date: 2/25/20
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+
+
+
+int main() {
+    cout << "hey";
+
+    return 0;
+}
